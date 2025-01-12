7f27fa5b34c804433ec75f0a0efeed7665672066
Orewagenki <orewagenki3@gmail.com>
2025-01-11 21:47:24 -0700

    build and styled the banner component.

---
+++ src/components/Banner.tsx
@@ -0,0 +1,75 @@
+import { Col, Container, Row } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { useEffect, useState } from "react";
+
+const Banner = () => {
+  const period = 2000;
+  const [text, setText] = useState("");
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Software Engineer", "Web Developer"];
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi I'm Joey `}
+              <span>{text}</span>
+            </h1>
+            <p>
+              {`Welcome to my website. I spend my free time building web applications. I find great joy in solving
+              various programming problems and feel like web development
+              scratches that itch. My Hobbies outside
+              of coding are jiu-jitsu, skateboarding, cooking, travel, and
+              language learning (I speak Japanese, English, and a little Italian).`}
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Let's connect
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Banner;
